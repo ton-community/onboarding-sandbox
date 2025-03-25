c1ff6802e9ed95bb3665fa031eea30cc5d42a305
Alexey Ostrovsky <a.ostrovsky@pixelplex.io>
2025-03-25 21:39:39 +0300

    deployment section examples

--- quick-start/smart-contracts/Example/scripts/deployCounterInternal.ts
+++ quick-start/smart-contracts/Example/scripts/deployCounterInternal.ts
@@ -1,9 +1,15 @@
 import { toNano } from '@ton/core';
 import { CounterInternal } from '../wrappers/CounterInternal';
 import { NetworkProvider } from '@ton/blueprint';
+import { Address } from '@ton/core';
 
 export async function run(provider: NetworkProvider) {
-    const counterInternal = provider.open(await CounterInternal.fromInit(BigInt(Math.floor(Math.random() * 10000))));
+    const counterInternal = provider.open(
+        await CounterInternal.fromInit(
+            BigInt(Math.floor(Math.random() * 10000)),
+            Address.parse("kQDW2lkFHPO_EWAsWI90MdvqU5fr8tiELbbcfaA8FmSkMVJ8") //just a random address
+        ),
+    );
 
     await counterInternal.send(
         provider.sender(),
--- quick-start/smart-contracts/Example/scripts/deployHelloWorld.ts
+++ quick-start/smart-contracts/Example/scripts/deployHelloWorld.ts
@@ -1,13 +1,19 @@
 import { toNano } from '@ton/core';
 import { HelloWorld } from '../wrappers/HelloWorld';
+import { CounterInternal } from '../wrappers/CounterInternal';
 import { compile, NetworkProvider } from '@ton/blueprint';
+import { mnemonicToWalletKey } from '@ton/crypto';
 
 export async function run(provider: NetworkProvider) {
+    const mnemonic = "table dizzy all reopen repeat tag august bid lunch purse able logic shuffle shoe ritual jacket dilemma cage dragon plunge arrange sister catalog impact".split(' '); // Insert your mnemonic
+    const { publicKey, secretKey } = await mnemonicToWalletKey(mnemonic);
+
     const helloWorld = provider.open(
         HelloWorld.createFromConfig(
             {
                 id: Math.floor(Math.random() * 10000),
-                counter: 0,
+                seqno: 0,
+                publicKey: publicKey
             },
             await compile('HelloWorld')
         )
@@ -17,5 +23,26 @@ export async function run(provider: NetworkProvider) {
 
     await provider.waitForDeploy(helloWorld.address);
 
+    const counterInternal = provider.open(
+        await CounterInternal.fromInit(
+            BigInt(Math.floor(Math.random() * 10000)),
+            helloWorld.address
+        )
+    );
+
+    await counterInternal.send(
+        provider.sender(),
+        {
+            value: toNano('0.05'),
+        },
+        {
+            $$type: 'Deploy',
+            queryId: 0n,
+        }
+    );
+
+    await provider.waitForDeploy(counterInternal.address);
+
     console.log('ID', await helloWorld.getID());
+    console.log('ID', (await counterInternal.getId()).toString());
 }
