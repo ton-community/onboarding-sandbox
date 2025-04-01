41ee3635d133df82bbd941f198af93fd360fee54
Alexey Ostrovsky <a.ostrovsky@pixelplex.io>
2025-04-01 22:39:43 +0300

    Few more fixes

--- quick-start/smart-contracts/Example/scripts/deployHelloWorld.ts
+++ quick-start/smart-contracts/Example/scripts/deployHelloWorld.ts
@@ -2,18 +2,14 @@ import { toNano } from '@ton/core';
 import { HelloWorld } from '../wrappers/HelloWorld';
 import { CounterInternal } from '../wrappers/CounterInternal';
 import { compile, NetworkProvider } from '@ton/blueprint';
-import { mnemonicToWalletKey } from '@ton/crypto';
 
 export async function run(provider: NetworkProvider) {
-    const mnemonic = "table dizzy all reopen repeat tag august bid lunch purse able logic shuffle shoe ritual jacket dilemma cage dragon plunge arrange sister catalog impact".split(' '); // Insert your mnemonic
-    const { publicKey, secretKey } = await mnemonicToWalletKey(mnemonic);
-
     const helloWorld = provider.open(
         HelloWorld.createFromConfig(
             {
                 id: Math.floor(Math.random() * 10000),
-                seqno: 0,
-                publicKey: publicKey
+                ctxCounter: 0,
+                ctxCounterExt: 0n,
             },
             await compile('HelloWorld')
         )
